ea0d879ca5f2474ebe4311fb3a5bd5c163a8185d
MichaelJames02 <michael4achieve@gmail.com>
2018-11-15 21:44:08 -0200

    Updated urls.py file

--- estate/urls.py
+++ estate/urls.py
@@ -1,11 +1,16 @@
-"""estate URL Configuration"""
-
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from estateapp.views import HomeView
+from django.conf import settings
+from django.conf.urls.static import static
+from estateapp import views
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^house/', views.HouseView.as_view(), name='house'),
+    url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
+    url(r'^uploads/form/$', views.form_photo_upload, name='form_photo_upload'),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
