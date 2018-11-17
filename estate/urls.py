38a46c2d7d6ee9b5e2441187c96b973bc6d8fb35
MichaelJames02 <michael4achieve@gmail.com>
2018-11-17 10:17:05 -0200

    urls.py update

--- estate/urls.py
+++ estate/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
@@ -11,11 +10,9 @@ urlpatterns = [
     url(r'^success/', views.success, name='success'),
     url(r'^service/', views.service, name='service'),
     url(r'^house/', views.HouseView.as_view(), name='house'),
-    url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
-    url(r'^uploads/form/$', views.form_photo_upload, name='form_photo_upload'),
-    path('admin/', admin.site.urls),
+    url(r'^form/photo_upload$', views.photo_upload, name='photo_upload'),
+    url(r'admin/', admin.site.urls),
 
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
