f4c110d9d669a53f3f85f11760e28adfaea3b7bc
MichaelJames02 <michael4achieve@gmail.com>
2018-11-17 08:33:04 -0200

    Added success url

--- estate/urls.py
+++ estate/urls.py
@@ -5,12 +5,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from estateapp import views
 
+
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^success/', views.success, name='success'),
+    url(r'^service/', views.service, name='service'),
     url(r'^house/', views.HouseView.as_view(), name='house'),
     url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
     url(r'^uploads/form/$', views.form_photo_upload, name='form_photo_upload'),
+    # path('account/documents/', views.DocumentsView.as_view(), name='core>documents'),
     path('admin/', admin.site.urls),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
