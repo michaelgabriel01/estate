fea722e072f243c0a8f2ec6b7ca79c2f7d231c4f
MichaelJames02 <michael4achieve@gmail.com>
2018-11-19 15:49:16 -0200

    Removed static url from the urls.py

--- estate/urls.py
+++ estate/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
 from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 from estateapp import views
 
 
@@ -15,5 +15,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
